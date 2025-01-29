6ee36dea0ef59967c20ee117df9fcea058218825
AyushSharma7067 <AyushSharma7067@gmail.com>
2025-01-29 09:52:53 +0530

    Update yt_video.py

--- TeleBOT/yt_video.py
+++ TeleBOT/yt_video.py
@@ -1,5 +1,5 @@
 import yt_dlp
-
+import os
 
 def fetch_video_formats(url):
     """
@@ -19,17 +19,13 @@ def fetch_video_formats(url):
                     filesize_bytes = fmt.get('filesize', None)
                     filesize_mb = f"{filesize_bytes / (1024 * 1024):.2f} MB" if filesize_bytes else "Unknown size"
                     video_formats.append({
-                        'format_id':
-                        fmt['format_id'],
-                        'format_note':
-                        fmt.get('format_note', 'Unknown'),
-                        'filesize':
-                        filesize_mb
+                        'format_id': fmt['format_id'],
+                        'format_note': fmt.get('format_note', 'Unknown'),
+                        'filesize': filesize_mb
                     })
             return video_formats
     except Exception as e:
         raise ValueError(f"Error fetching formats: {str(e)}")
-
 
 def download_video_with_audio(url, format_id, output_dir='downloads'):
     """
@@ -45,9 +41,8 @@ def download_video_with_audio(url, format_id, output_dir='downloads'):
         ydl_opts = {
             'quiet': True,
             'outtmpl': f'{output_dir}/%(title)s.%(ext)s',
-            'format':
-            f'{format_id}+bestaudio/best',  # Combine video and best audio
-            'merge_output_format': 'mp4'  # Merge into MP4 format
+            'format': f'{format_id}+bestaudio/best',  # Combine video and best audio
+            'merge_output_format': 'mp4',  # Merge into MP4 format
             'cookiefile': cookies_path
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
